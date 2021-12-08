d28e6b84638e9d21b8ff99ffa85e61351c803b4c
Ragan Suarez <ragansuarez@Ragans-MacBook-Pro.local>
2021-12-07 19:54:34 -0700

    updates to appendix code

--- jura_spatial_cokrige_appendix.R
+++ jura_spatial_cokrige_appendix.R
@@ -12,26 +12,27 @@ summary(transect.dat)
 summary(juragrid.dat)
 
 # rename pred and grid
-jura.pred = prediction.dat
-jura.grid = juragrid.dat
+jura.pred <- prediction.dat
+jura.grid <- juragrid.dat
 
 # give labels to integer values 
-jura.pred$Landuse = factor(prediction.dat$Landuse, 
-                           labels=levels(juragrid.dat$Landuse))
-jura.pred$Rock = factor(prediction.dat$Rock, 
-                        labels=levels(juragrid.dat$Rock))
+jura.pred$Landuse <- factor(prediction.dat$Landuse, 
+                           labels = levels(juragrid.dat$Landuse))
+jura.pred$Rock <- factor(prediction.dat$Rock, 
+                        labels = levels(juragrid.dat$Rock))
 
 # convert to spatial dataframes
-coordinates(jura.pred) = ~Xloc+Yloc
-coordinates(jura.grid) = ~Xloc+Yloc
-gridded(jura.grid) = TRUE
+coordinates(jura.pred) <- ~Xloc + Yloc
+coordinates(jura.grid) <- ~Xloc + Yloc
+gridded(jura.grid) <- TRUE
 
 # sample plot of Cd
 spplot(jura.pred, 'Cd', main = "Cd Concentration Levels")
 
 # using inverse distance weighting method to predict values for Cd
 cad.raw <- idw(Cd ~ 1, jura.pred, jura.grid)
-spplot(cad.raw['var1.pred'], main = "Predicted Cd Concentration (using inverse distance)")
+spplot(cad.raw['var1.pred'], 
+       main = "Predicted Cd Concentration (inverse distance)")
 
 # looking for correlations between Cd and predictor variables
 # nickel
@@ -54,7 +55,9 @@ plot(log(Cd) ~ log(Zn), jura.pred)
 
 # cokriging of Cd with Zn
 # visualize the feature-space correlation 
-ggplot(as.data.frame(jura.pred), aes(x = Zn, y = Cd)) + 
+ggplot(as.data.frame(jura.pred), 
+       aes(x = Zn, 
+           y = Cd)) + 
   geom_point(col = "blue", alpha = 0.5, size = 2.5) + 
   geom_smooth(col = "grey", se = F, method = "lm") + 
   theme_bw() + 
@@ -62,7 +65,9 @@ ggplot(as.data.frame(jura.pred), aes(x = Zn, y = Cd)) +
   xlab("Zinc") +
   ylab("Cadmium")
 
-ggplot(as.data.frame(jura.pred), aes(x = log(Zn), y = log(Cd))) + 
+ggplot(as.data.frame(jura.pred), 
+       aes(x = log(Zn), 
+           y = log(Cd))) + 
   geom_point(col = "blue", alpha = 0.5, size = 2.5) + 
   geom_smooth(col = "grey", se = F, method = "lm") + 
   theme_bw() + 
@@ -72,8 +77,14 @@ ggplot(as.data.frame(jura.pred), aes(x = log(Zn), y = log(Cd))) +
 
 # create a gstat object for cokriging
 # add data
-jura.g <- gstat(NULL, id = "Cd", formula = log(Cd) ~ 1, data = jura.pred, nmax = 10)
-jura.g <- gstat(jura.g, id = "Zn", formula = log(Zn) ~ 1, data = jura.pred, nmax = 10)
+jura.g <- gstat(NULL, id = "Cd", 
+                formula = log(Cd) ~ 1, 
+                data = jura.pred, 
+                nmax = 10)
+jura.g <- gstat(jura.g, id = "Zn", 
+                formula = log(Zn) ~ 1, 
+                data = jura.pred, 
+                nmax = 10)
 
 # compute the experimental variograms and cross-variogram
 x <- variogram(jura.g)
@@ -83,19 +94,25 @@ jura.fit <- fit.lmc(x, jura.g, vgm(1, "Exp", 1.2, 1))
 plot(x, jura.fit)
 
 # add model
-jura.g <- gstat(jura.g, model = vgm(1, "Exp", 1.2, 1), fill.all = T)
+jura.g <- gstat(jura.g, 
+                model = vgm(1, "Exp", 1.2, 1), 
+                fill.all = T)
 
 # cokriging
 jura.g <- fit.lmc(x, jura.g)
 cz <- predict(jura.g, jura.grid)
 
 # plot the prediction results
-cz$Cd.se = sqrt(cz$Cd.var)
-cz$Zn.se = sqrt(cz$Zn.var)
-pl1 = spplot(cz["Cd.pred"], main = "log-cadmium prediction")
-pl2 = spplot(cz["Zn.pred"], main = "log-zinc prediction")
-pl3 = spplot(cz["Cd.se"], main = "log-cadmium standard error")
-pl4 = spplot(cz["Zn.se"], main = "log-zinc standard error")
+cz$Cd.se <- sqrt(cz$Cd.var)
+cz$Zn.se <- sqrt(cz$Zn.var)
+pl1 <- spplot(cz["Cd.pred"], 
+              main = "log-cadmium prediction")
+pl2 <- spplot(cz["Zn.pred"], 
+              main = "log-zinc prediction")
+pl3 <- spplot(cz["Cd.se"], 
+              main = "log-cadmium standard error")
+pl4 <- spplot(cz["Zn.se"], 
+              main = "log-zinc standard error")
 
 grid.arrange(pl1,pl2,pl3,pl4, nrow = 2)
 
